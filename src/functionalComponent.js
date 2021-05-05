f2e2bf22ed7f3500e9943369d871ea98a14be337
Saurabh Kumar <v-saukumar@microsoft.com>
2021-05-05 11:35:46 +0530

    Implemented product add functionality

--- src/functionalComponent.js
+++ src/functionalComponent.js
@@ -1,38 +1,101 @@
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Table } from "react-bootstrap";
+import { useState, createRef } from "react";
 
 export default function AddProduct() {
-    const addProductData = (event) => {
-        event.preventDefault();
-        console.log(event);
-    }
+  const initialValue = [];
+  const [products, setProducts] = useState(initialValue);
+  const formData = createRef();
 
-    return (
-        <Form onSubmit={addProductData}>
-            <Form.Group controlId="formName">
-                <Form.Label>Name:</Form.Label>
-                <Form.Control name="product_name" type="text" placeholder="Enter Product Name"></Form.Control>
-            </Form.Group>
+  const addProductData = (event) => {
+    event.preventDefault();
+    const newProduct = {
+      product_name: formData.current.product_name.value,
+      product_description: formData.current.product_description.value,
+      product_price: Number(formData.current.product_price.value),
+      product_quantity: Number(formData.current.product_quantity.value),
+    };
 
+    setProducts([...products, newProduct]);
+  };
+
+  console.log(products);
+
+  return (
+    <>
+      <Form onSubmit={addProductData} ref={formData}>
+        <Form.Group controlId="formName">
+          <Form.Label>Name:</Form.Label>
+          <Form.Control
+            name="product_name"
+            type="text"
+            placeholder="Enter Product Name"
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="formDescription">
+          <Form.Label>Description:</Form.Label>
+          <Form.Control
+            name="product_description"
+            type="text"
+            placeholder="Enter Product Description"
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="formPrice">
+          <Form.Label>Price:</Form.Label>
+          <Form.Control
+            name="product_price"
+            type="decimal"
+            placeholder="Enter Product Price"
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="formQuantity">
+          <Form.Label>Quantity:</Form.Label>
+          <Form.Control
+            name="product_quantity"
+            type="number"
+            placeholder="Enter Product Quantity"
+          ></Form.Control>
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Add to Inventory
+        </Button>
+      </Form>
+    <br />
+      {products.length > 0 && (
+        <Table striped bordered hover variant="dark">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Price</th>
+              <th>Quantity</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+              {
+                products.map((item, index) => {
+                    return(
+                        <tr key={++index}>
+                            <td>{++index}</td>
+                            <td>{item.product_name}</td>
+                            <td>{item.product_description}</td>
+                            <td>{item.product_price}</td>
+                            <td>{item.product_quantity}</td>
+                            <td></td>
+                        </tr>
+                        )
+                    })
+                }   
             
-            <Form.Group controlId="formDescription">
-                <Form.Label>Description:</Form.Label>
-                <Form.Control name="product_description" type="text" placeholder="Enter Product Description"></Form.Control>
-            </Form.Group>
-
-            <Form.Group controlId="formPrice">
-                <Form.Label>Price:</Form.Label>
-                <Form.Control name="product_price" type="number" placeholder="Enter Product Price"></Form.Control>
-            </Form.Group>
-
-            
-            <Form.Group controlId="formName">
-                <Form.Label>Quantity:</Form.Label>
-                <Form.Control name="product_quantity" type="number" placeholder="Enter Product Quantity"></Form.Control>
-            </Form.Group>
-
-            <Button variant="primary" type="submit">
-                Add to Inventory
-            </Button>
-        </Form>
-    )
+          </tbody>
+        </Table>
+      )}
+      <br />
+    </>
+  );
 }
